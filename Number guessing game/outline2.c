d077fbfdfaa60888d95dd4d89976188ccb8c8179
Laura Siu <lmsiu@cpp.edu>
2021-09-23 22:20:00 -0700

    Added outline

---
+++ Number guessing game/outline2.c
@@ -0,0 +1,7 @@
+/*Number Guessing Game outline:
+1. Set main() with return type void
+2. Create menu promt (scanf)
+3. Create if/switch stament for menu
+4. Set up options
+5. Set up 'q' functionality
+6. save_user_max_number*/
---
+++ Restaurant Bill/outline.c
@@ -0,0 +1,9 @@
+/*Restaurant Bill outline:
+
+1. Set up main() with return type void
+2. Set up vars
+3. Write code to accept args from the command line for the tax and tip
+4. Set up random pick for meal
+5. Calculate tax and tip
+6. Print results to screen
+*/
